7198c06883cf6628b3a963eb5b4b2f2c514bc640
cheadrian <bla@bum.com>
2021-11-05 17:39:01 -0400

    Add vivante DRM from iMX6

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 0 */
+/* This file is auto generated, version 1 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#0 SMP PREEMPT Thu Nov 4 12:23:35 EDT 2021"
+#define UTS_VERSION "#1 SMP PREEMPT Fri Nov 5 17:35:24 EDT 2021"
 #define LINUX_COMPILE_BY "osboxes"
 #define LINUX_COMPILE_HOST "osboxes"
 #define LINUX_COMPILER "gcc version 4.7.4 (Ubuntu/Linaro 4.7.4-3ubuntu12) "
